560a250c59b8db468fd62930893239cfc9db7c6e
banndit <feles.catt@gmail.com>
2020-03-17 12:39:26 +0700

    env

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import config from '../config';
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID ${config.ACCESS_KEY}`
+    Authorization:
+      'Client-ID ' +
+      '5112106a9ee88feb947399bb01247f3cc83b62edd88e4852265ca98f64f3102b'
   }
 });
