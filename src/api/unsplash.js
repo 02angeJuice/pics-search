17a9d9f4f5afe975cd92cce6a2ac48609e20f281
banndit <feles.catt@gmail.com>
2020-03-17 07:10:06 +0700

    Added ignore

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
-
-const ACCESS_KEY =
-  '5112106a9ee88feb947399bb01247f3cc83b62edd88e4852265ca98f64f3102b';
+import config from '../config';
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID ${ACCESS_KEY}`
+    Authorization: `Client-ID ${config.ACCESS_KEY}`
   }
 });
